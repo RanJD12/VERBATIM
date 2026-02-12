46a63a94920c806752133817ba2374b016cf4afd
RanJD12 <officialranjd@gmail.com>
2026-02-12 18:57:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Empty for now to pass validation
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
